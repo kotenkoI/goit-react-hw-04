2ed3772615ff866285929c20474e5b7403c0421f
kotenkoI <iryna.herashchenko@gmail.com>
2024-06-10 15:01:29 +0300

    updated

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,14 +1,22 @@
 import { Audio } from 'react-loader-spinner';
 export default function Loader(){
-
+    const loaderStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh', 
+    };
     return(
-        <div>
+        <div style={loaderStyle}>
         
         <Audio
-  height="80"
-  width="80"
-  radius="9"
-  color="blue"
+  height="40"
+  width="40"
+          radius="8"
+          
+          
+  color="green"
   ariaLabel="loading"
   wrapperStyle
   wrapperClass
