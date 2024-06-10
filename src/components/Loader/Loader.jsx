e9fedbd567603df317b6793d1364485081682eb8
kotenkoI <iryna.herashchenko@gmail.com>
2024-06-10 14:47:17 +0300

    added

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,20 @@
+import { Audio } from 'react-loader-spinner';
+export default function Loader(){
+
+    return(
+        <div>
+        
+        <Audio
+  height="80"
+  width="80"
+  radius="9"
+  color="blue"
+  ariaLabel="loading"
+  wrapperStyle
+  wrapperClass
+/>
+<p>Loading data, please wait...</p>
+        </div>
+
+    )
+}
